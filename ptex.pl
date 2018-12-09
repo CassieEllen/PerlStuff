5dba170816a6e62bcf8fff2f97ab69b061b8b088
Cassie Nicol <cassieenicol@gmail.com>
2018-12-09 09:21:52 -0600

    simple data dump

---
+++ ptex.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+# sudo apt install -y libproc-processtable-perl
+use strict;
+
+use warnings;
+
+use Proc::ProcessTable;
+
+use Data::Dumper;
+
+
+my $p = Proc::ProcessTable->new( 'cache_ttys' => 1 );
+print Dumper $p;
+    
+my @fields = $p->fields;
+print Dumper @fields;
+
+my $ref = $p->table;
+print Dumper $ref;
+
